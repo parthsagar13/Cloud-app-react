2312259f06b2c45fa61baa76537656aed414909e
Parth Sagar <kallathiyaparth744@gmail.com>
2023-01-29 02:04:00 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './Componets/About';
+import Hero from './Componets/Hero';
+import Navbar from './Componets/Navbar';
+import Support from './Componets/Support';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Navbar/>
+        <Hero/>
+        <About/>
+        <Support/>
+    </>
   );
 }
 
