791077c36c8d2ae38f808457ab47af3f568f3ded
Parth Sagar <kalathiyaparth744@gmail.com>
2024-02-25 09:37:34 +0530

    ---Add new Component

--- src/Components/All Routes/AllRoutes.jsx
+++ src/Components/All Routes/AllRoutes.jsx
@@ -1,15 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import Home from "../Home/Home";
-import Products from "../Products/Products";
-import { ProductS } from "../Products/Productss";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
 import Address from "../Address/Address";
 import Payment from "../Payment/Payment";
 import Summary from "../Summary/Summary";
-import Profile from "../Profile/Profile";
-import Login from "../Profile/Login";
 import Footer from "../Footer/Footer";
 import Checkout from "../Checkout/Checkout";
 
@@ -19,13 +15,8 @@ export default function AllRoutes() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
         <Route path="/product" element={<Product />} />
-        <Route path="/products/:type" element={<Products />} />
-        <Route path="/products/:type/:subtype" element={<ProductS />} />
         <Route path="/product/:id" element={<Product />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/login" element={<Login />} />
         <Route
           path="/checkout/cart"
           element={
@@ -63,7 +54,6 @@ export default function AllRoutes() {
           }
         />
       </Routes>
-
       <Footer />
     </>
   );
