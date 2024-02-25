fb8df95d9e0bb672f8ce1268714e85367153574e
RajTjcg <rajantala240@gmail.com>
2024-02-25 16:23:28 +0530

    --sidebar

--- src/Components/Home/SliderComponent/SliderComponent.jsx
+++ src/Components/Home/SliderComponent/SliderComponent.jsx
@@ -1,53 +1,42 @@
-// import React from 'react';
-// import Typography from '@material-ui/core/Typography';
-// import Box from '@material-ui/core/Box';
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-// // Import Swiper styles
-// import 'swiper/swiper.min.css';
-// import 'swiper/components/navigation/navigation.min.css';
+// Import Swiper styles
+import 'swiper/css';
 
+const sliederComponent = () => {
 
-// // import Swiper core and required modules
-// import SwiperCore, {
-//   Navigation,
-//   Autoplay // Import Autoplay module
-// } from 'swiper/core';
+    const images = [
+        'https://kalamandir.online/static/media/banner2.4a2dc8f1d7e54e393e66.webp',
+        'https://kalamandir.online/static/media/banner4.f04dcbd764616e16886f.webp',
+        'https://kalamandir.online/static/media/banner3.c244679043edcc839c9e.webp',
+        'https://kalamandir.online/static/media/banner5.1f1f8819d9a998d2ec63.webp',
+    ];
 
-// // install Swiper modules
-// SwiperCore.use([Navigation, Autoplay]); // Add Autoplay module to SwiperCore
+    const swiperStyle = {
+        width: '100%',
+        height: '100%',
+    };
 
-// export default function App() {
-//   return (
-//     <Box className="App" padding={2}>
-//       <Typography
-//         variant={'h4'}
-//         align={'center'}
-//         fontWeight={700}
-//       >
-//         Swiper + Material-UI example
-//       </Typography>
-//       {/** Slider main container */}
-//       <Box marginTop={4}>
-//         <Swiper 
-//           navigation={true} 
-//           autoplay={{ delay: 3000 }} // Specify autoplay delay in milliseconds
-//           className="mySwiper"
-//         >
-//           {/** Slides */}
-//           {['Slide 1', 'Slide 2', 'Slide 3'].map((item, i) => (
-//             <SwiperSlide key={i}>
-//               <Typography
-//                 variant={'h6'}
-//                 align={'center'}
-//               >
-//                 {item}
-//               </Typography>
-//             </SwiperSlide>
-//           ))}
-//         </Swiper>
-//       </Box>
-//     </Box>
-//   );
-// }
+    const imageStyle = {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+    };
+
+    return (
+        <Swiper
+            style={swiperStyle}
+            spaceBetween={50}
+            slidesPerView={1}
+            autoplay={{ delay: 1000 }}
+        >
+            {images.map((image, index) => (
+                <SwiperSlide key={index}>
+                    <img style={imageStyle} src={image} alt={`Slide ${index + 1}`} />
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+};
+
+export default sliederComponent
